65e6981fd3e1cf9fea858b9ddbec30eab3d63fa9
Hanan Shoubaki <hananshoubaki@gmail.com>
2025-03-10 20:49:24 -0500

    updated footer content

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -22,17 +22,6 @@ export default {
     }
   ],
   bottom: [
-    {
-      text: 'Style guide',
-      url: '/styleguide/'
-    },
-    {
-      text: 'Imprint',
-      url: '/imprint/'
-    },
-    {
-      text: 'Privacy',
-      url: '/privacy/'
-    }
+    
   ]
 };
